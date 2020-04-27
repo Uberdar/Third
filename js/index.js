fbd1c924a59888d977ceac5bced866a9fef1c571
Uberdar <allexvik@gmail.com>
2020-04-27 17:04:59 +0300

    1

--- js/index.js
+++
@@ -1,8 +0,0 @@
-function rotateFunction() {
-var x = document.getElementById("ishallrotate");
-  x.style.transition = "transform 1s";
-  x.style.transformOrigin = "25% 70%";
-  if (x.style.transform === "rotate(0deg)") {
-  x.style.transform = "rotate(180deg)";
-  } else {x.style.transform = "rotate(0deg)" }
-}
